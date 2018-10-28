323df42ce960ed4cd36c3b06243eb6a2bb722cd1
张杨 <zhangyang.tt@bytedance.com>
2018-10-28 18:12:12 +0800

    Initial commite.
    Change-Id: I06fa2fb6f6c96a25bf9a1b5e7720c88a302e2724

---
+++ conda_install.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# Help messages:
+usage="$(basename "$0") [-h] -- program to install conda packages based on requirements.txt file.
+
+where:
+    -h  show this help text"
+
+seed=42
+while getopts ':hs:' option; do
+  case "$option" in
+    h) echo "$usage"
+       exit
+       ;;
+    s) seed=$OPTARG
+       ;;
+    :) printf "missing argument for -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+   \?) printf "illegal option: -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+# The main program starts from here:
+while read line; do
+  if [[ ${line} =~ ^\#[[:space:]]+-c[[:space:]]+conda-forge ]];then
+    chanel=${line:1}
+  elif [[ ${line} =~ ^\# ]];then
+    continue
+  else
+    echo "conda install $chanel ${line}"
+    conda install ${chanel} ${line}
+    chanel=""
+  fi
+done <requirements.txt
